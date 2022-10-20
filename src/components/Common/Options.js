71d22d70bb0da0283af4d2e14e5361887263a556
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-10-20 21:09:40 +0300

    fix

---
+++ src/components/Common/Options.js
@@ -0,0 +1,7 @@
+export const options = [
+  { key: 'good', cc: 'Good', uc: 'GOOD', lc: 'good', bgColor: '#ccffcc' },
+  { key: 'ok', cc: 'Ok', uc: 'OK', lc: 'ok', bgColor: '#e8ffe8' },
+  { key: 'neutral', cc: 'Neutral', uc: 'NEUTRAL', lc: 'neutral', bgColor: '#ccffff' },
+  { key: 'notgood', cc: 'Not good', uc: 'NOT GOOD', lc: 'not good', bgColor: '#ffe8e8' },
+  { key: 'bad', cc: 'Bad', uc: 'BAD', lc: 'bad', bgColor: '#ffcccc' },
+];
